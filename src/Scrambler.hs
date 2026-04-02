c40ce032e61f8fedbfbb7ab77d2db6d8bf9b6ec9
Zeno Leonardi <zenoelioleone@gmail.com>
2026-04-02 02:29:33 +0200

    add no consecutive opposites on scrambler

--- src/Scrambler.hs
+++ src/Scrambler.hs
@@ -1,4 +1,4 @@
-module Scrambler (scramble, generateMoves) where
+module Scrambler (scramble, generateMoves, opposites) where
 import Move (Move(..))
 import System.Random
 {-
@@ -25,10 +25,12 @@ generateMoves (prev1, prev2) n = do
     randomMove <- getRandomMove
 
     if prev1 == Just randomMove && prev2 == Just randomMove
+        then generateMoves (prev1, prev2) n
+    else if opposites prev2 ( Just randomMove )
         then generateMoves (prev1, prev2) n 
-        else do
-            rest <- generateMoves (prev2, Just randomMove) (n-1)
-            return (randomMove : rest) 
+    else do
+        rest <- generateMoves (prev2, Just randomMove) (n-1)
+        return (randomMove : rest) 
 
 
 getRandomMove :: IO Move
@@ -36,6 +38,17 @@ getRandomMove = do
     randomNum <- randomRIO (0, 5) :: IO Int
     return (numberToMove randomNum)
 
+opposite :: Move -> Move
+opposite U = D
+opposite D = U 
+opposite L = R 
+opposite R = L 
+opposite B = F 
+opposite F = B 
+
+opposites :: Maybe Move -> Maybe Move -> Bool
+opposites (Just m1) ( Just m2 ) = m1 == opposite m2
+opposites _ _ = False
 
 numberToMove :: Int -> Move
 numberToMove 0 = U
--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 import Move (Move(..))
-import Scrambler (generateMoves)
+import Scrambler (generateMoves, opposites)
 import Test.QuickCheck
 
 instance Arbitrary Move where
@@ -11,9 +11,17 @@ main = do
     quickCheck $ \n -> ioProperty $ do
         moves <- generateMoves (Nothing, Nothing) (abs n + 1)
         return (noTriplet moves)
+    quickCheck $ \n -> ioProperty $ do
+      moves <- generateMoves (Nothing, Nothing) (abs n + 1)
+      return (noConsecutiveOpposites moves)
 
 noTriplet :: [Move] -> Bool
 noTriplet [] = True
 noTriplet [_] = True
 noTriplet [_,_] = True
 noTriplet (a:b:c:rest) = if a == c && b == c then False else noTriplet rest 
+
+noConsecutiveOpposites :: [Move] -> Bool
+noConsecutiveOpposites [] = True
+noConsecutiveOpposites [_] = True
+noConsecutiveOpposites (a:b:rest) = if opposites (Just a) (Just b) then False else noConsecutiveOpposites (b:rest)
