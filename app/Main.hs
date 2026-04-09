490d0e80e6c0bd6bdcea2df12be60798b57ea76d
Zeno Leonardi <zenoelioleone@gmail.com>
2026-04-09 13:47:50 +0200

    Change datastructure for cube

--- app/Main.hs
+++ app/Main.hs
@@ -5,50 +5,10 @@ import qualified Scrambler (scramble)
 import Model 
 import Display
 
-solvedCube :: Cube 
-solvedCube = Cube {
-  left = orangeFace,
-  up = yellowFace,
-  front = greenFace,
-  down = whiteFace,
-  right = redFace,
-  back = blueFace
-} 
-
-greenFace :: Face
-greenFace = Face (Row Green Green Green)
-                 (Row Green Green Green)
-                 (Row Green Green Green)
-
-yellowFace :: Face
-yellowFace = Face (Row Yellow Yellow Yellow)
-                  (Row Yellow Yellow Yellow)
-                  (Row Yellow Yellow Yellow)
-
-redFace :: Face
-redFace = Face (Row Red Red Red)
-               (Row Red Red Red)
-               (Row Red Red Red)
-
-whiteFace :: Face
-whiteFace = Face (Row White White White)
-                 (Row White White White)
-                 (Row White White White)
-
-blueFace:: Face
-blueFace = Face (Row Blue Blue Blue)
-                (Row Blue Blue Blue)
-                (Row Blue Blue Blue)
-
-orangeFace :: Face
-orangeFace = Face (Row Orange Orange Orange)
-                  (Row Orange Orange Orange)
-                  (Row Orange Orange Orange)
-
 main :: IO ()
 main = do
   args <- getArgs
-  case args of 
+  case args of
     ("scramble":numberOfMoves:_) -> Scrambler.scramble ( read numberOfMoves :: Int )
     ("scramble":_) -> Scrambler.scramble 25
     ("show":_) -> putStrLn (show solvedCube)
--- src/Model.hs
+++ src/Model.hs
@@ -1,28 +1,73 @@
-module Model (Cube(..), Face(..), Row(..), Color(..)) where
+data Cube = Cube Corners Edges
 
-data Cube = 
-    Cube {
-        up :: Face,
-        down :: Face, 
-        front :: Face, 
-        back :: Face, 
-        left :: Face, 
-        right :: Face
+data Corners = Corners
+      { urf :: CornerCubie
+      , ulf :: CornerCubie                                                      
+      , urb :: CornerCubie
+      , ulb :: CornerCubie                                                      
+      , drf :: CornerCubie                                                      
+      , dlf :: CornerCubie                                                     
+      , drb :: CornerCubie                                                      
+      , dlb :: CornerCubie
+      }                                                                         
+
+data CornerCubie = CornerCubie CornerPosition CornerOrientaion 
+
+data CornerPosition = URF | ULF | URB | ULB | DRF | DLF | DRB | DLB
+
+data CornerOrientaion = CNeutral | CW | CCW 
+
+data Edges = Edges                                                            
+    { uf :: EdgeCubie
+    , ur :: EdgeCubie                                                        
+    , ul :: EdgeCubie
+    , ub :: EdgeCubie
+    , df :: EdgeCubie
+    , dr :: EdgeCubie                                                         
+    , dl :: EdgeCubie
+    , db :: EdgeCubie                                                         
+    , rf :: EdgeCubie
+    , lf :: EdgeCubie                                                        
+    , rb :: EdgeCubie
+    , lb :: EdgeCubie                                                         
     }
-    deriving (Eq) 
+
+data EdgeCubie = EdgeCubie EdgePosition EdgeOrientation
+
+data EdgePosition = UF | UR | UL | UB | DF | DR | DL | DB | RF | LF | RB | LB
+
+data EdgeOrientation = ENeutral | EFlipped
 
 
-data Face = 
-    Face Row Row Row
-    deriving (Eq)
+solvedCorners :: Corners
+solvedCorners = Corners
+    { urf = CornerCubie URF CNeutral
+    , ulf = CornerCubie ULF CNeutral
+    , urb = CornerCubie URB CNeutral
+    , ulb = CornerCubie ULB CNeutral
+    , drf = CornerCubie DRF CNeutral
+    , dlf = CornerCubie DLF CNeutral
+    , drb = CornerCubie DRB CNeutral
+    , dlb = CornerCubie DLB CNeutral
+    }
+
+solvedEdges :: Edges
+solvedEdges = Edges
+    { uf = EdgeCubie UF ENeutral
+    , ur = EdgeCubie UR ENeutral
+    , ul = EdgeCubie UL ENeutral
+    , ub = EdgeCubie UB ENeutral
+    , df = EdgeCubie DF ENeutral
+    , dr = EdgeCubie DR ENeutral
+    , dl = EdgeCubie DL ENeutral
+    , db = EdgeCubie DB ENeutral
+    , rf = EdgeCubie RF ENeutral
+    , lf = EdgeCubie LF ENeutral
+    , rb = EdgeCubie RB ENeutral
+    , lb = EdgeCubie LB ENeutral
+    }
 
 
+solvedCube :: Cube
+solvedCube = Cube solvedCorners solvedEdges
 
-data Row = 
-    Row Color Color Color
-    deriving (Eq)
-
-
-data Color =
-    Red | Blue | Yellow | White | Green | Orange
-    deriving (Eq)
